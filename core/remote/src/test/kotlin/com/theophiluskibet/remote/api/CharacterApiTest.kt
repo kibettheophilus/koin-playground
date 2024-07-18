2f414c881086f30f0723fa9cf028192d7ccf9261
theophiluskibet <kibettheophilus@gmail.com>
2024-07-18 14:16:24 +0300

    create CharacterApiTest class

---
+++ core/remote/src/test/kotlin/com/theophiluskibet/remote/api/CharacterApiTest.kt
@@ -0,0 +1,8 @@
+package com.theophiluskibet.remote.api
+
+import com.theophiluskibet.remote.client.provideHttpClient
+import com.theophiluskibet.remote.di.RemoteModule
+
+class CharacterApiTest {
+
+}
