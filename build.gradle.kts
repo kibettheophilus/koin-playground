d6cb01be4aac159cd549426b6a715502556939a0
theophiluskibet <kibettheophilus@gmail.com>
2024-07-22 14:45:18 +0300

    feat: add spotless and license headers
    This commit adds spotless plugin and license headers to all files.
    
    - Added spotless plugin to the project.
    - Config
    ured spotless to format Kotlin files and add license headers.
    - Added license headers to all files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.spotless)
 }
 
 allprojects {
@@ -20,3 +21,25 @@ allprojects {
         }
     }
 }
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${project.rootDir}/build-logic/**/*.kt",)
+            licenseHeaderFile(
+                rootProject.file("${project.rootDir}/spotless/copyright.kt"),
+                "^(package|object|import|interface)"
+            )
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
+    }
+}
+
+
