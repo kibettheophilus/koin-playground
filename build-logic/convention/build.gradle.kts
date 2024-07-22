dd1cbad2e3a4d851df77ac5395d6d94551428a07
theophiluskibet <kibettheophilus@gmail.com>
2024-07-22 14:57:55 +0300

    setup build logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
