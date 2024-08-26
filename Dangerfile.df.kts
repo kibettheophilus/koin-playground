db8469b1f239730d1fe4ca89425e86d5ab59ed6f
theophiluskibet <kibettheophilus@gmail.com>
2024-08-26 20:32:40 +0300

    update boolean

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -3,7 +3,7 @@ import systems.danger.kotlin.*
 danger(args) {
     onGitHub {
         // Work in progress check
-        if (pullRequest.title.contains("WIP", false)) {
+        if (pullRequest.title.contains("WIP", true)) {
             warn("PR is classed as Work in Progress")
         }
     }
